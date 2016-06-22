2c87ab10e4b4cfa1bcc2bbdf86504e27d3fe2de3
rncbc <rncbc@gamma.rncbc.lan>
2016-06-22 22:21:03 +0100

    - Silenced weird g++6 compiler warning messages re. indentation, wtf. (2)

--- src/qmidinetOptions.h
+++ src/qmidinetOptions.h
@@ -1,7 +1,7 @@
 // qmidinetOptions.h
 //
 /****************************************************************************
-   Copyright (C) 2010-201, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2010-2016, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
