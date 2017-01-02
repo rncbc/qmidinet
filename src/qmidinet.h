57023f153a9a6a040184d86eeff58b1ba1e30c89
rncbc <rncbc@rncbc.org>
2017-01-02 11:01:30 +0000

    - Update to brand New Hear (2017).

--- src/qmidinet.h
+++ src/qmidinet.h
@@ -1,7 +1,7 @@
 // qmidinet.h
 //
 /****************************************************************************
-   Copyright (C) 2010-2016, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2010-2017, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -141,3 +141,4 @@ private:
 #endif	// __qmidinet_h
 
 // end of qmidinet.h
+
