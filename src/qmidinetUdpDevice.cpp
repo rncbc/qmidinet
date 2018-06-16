e03c9f400518d322b76b4c366b2eaf40d232e0dd
rncbc <rncbc@rncbc.org>
2018-06-16 13:04:46 +0100

    - Fixed for some g++ >= 8.1.1 warnings and quietness.

--- src/qmidinetUdpDevice.cpp
+++ src/qmidinetUdpDevice.cpp
@@ -1,7 +1,7 @@
 // qmidinetUdpDevice.cpp
 //
 /****************************************************************************
-   Copyright (C) 2010-2017, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2010-2018, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -437,7 +437,7 @@ bool qmidinetUdpDevice::get_address (
 #if !defined(__WIN32__) && !defined(_WIN32) && !defined(WIN32)
 
 	struct ifreq ifr;
-	::strncpy(ifr.ifr_name, ifname, sizeof(ifr.ifr_name));
+	::strncpy(ifr.ifr_name, ifname, sizeof(ifr.ifr_name) - 1);
 
 	if (::ioctl(sock, SIOCGIFFLAGS, (char *) &ifr)) {
 		::perror("ioctl(SIOCGIFFLAGS)");
